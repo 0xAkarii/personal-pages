4fc5f122bfb60f2703b849f74f3ed5cf20d19297
Alva Radian <radian1337.id@gmail.com>
2024-12-11 09:06:57 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Toggle class active
-
 const navbarNav = document.querySelector('.navbar-nav');
 
 document.querySelector('#hamburger-menu').onclick = (e) => {
@@ -8,12 +7,20 @@ document.querySelector('#hamburger-menu').onclick = (e) => {
 };
 
 // Click outside sidebar
-
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 
 document.addEventListener('click', function(e) {
     if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
-        e.preventDefault();
     }
 });
+
+// Hide sidebar when user click on links
+document.addEventListener('click', function(e) {
+    if (window.innerWidth <= 768){
+        if(navbarNav.contains(e.target)) {
+            navbarNav.classList.remove('active');
+        }
+    }
+})
+
